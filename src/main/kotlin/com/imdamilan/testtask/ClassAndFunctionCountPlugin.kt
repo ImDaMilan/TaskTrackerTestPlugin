12856ed2096d14a7dbe37c5243c9780da653ca9e
ImDaMilan <imdamilan333@gmail.com>
2023-04-29 12:50:35 +0200

    Finished

--- src/main/kotlin/com/imdamilan/testtask/ClassAndFunctionCountPlugin.kt
+++ src/main/kotlin/com/imdamilan/testtask/ClassAndFunctionCountPlugin.kt
@@ -2,18 +2,23 @@ package com.imdamilan.testtask
 
 import com.intellij.openapi.actionSystem.DefaultActionGroup
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.openapi.vfs.VirtualFileEvent
+import com.intellij.openapi.vfs.VirtualFileListener
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.psi.JavaRecursiveElementVisitor
+import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiManager
+import com.intellij.psi.PsiMethod
 import com.intellij.ui.components.JBScrollPane
 import com.intellij.ui.content.ContentFactory
-import com.intellij.ui.content.ContentManagerEvent
-import com.intellij.ui.content.ContentManagerListener
 import com.intellij.ui.treeStructure.Tree
 import java.awt.BorderLayout
-import java.io.File
 import javax.swing.JPanel
 import javax.swing.tree.DefaultMutableTreeNode
 import javax.swing.tree.DefaultTreeModel
+
 
 class ClassAndFunctionCountPlugin : ToolWindowFactory {
 
@@ -28,27 +33,41 @@ class ClassAndFunctionCountPlugin : ToolWindowFactory {
 
         toolWindow.contentManager.addContent(contentFactory.createContent(panel, "", false))
 
-        val virtualFiles = project.basePath?.let { basePath ->
-            File(basePath).walkTopDown().filter { it.extension == "kt" }
-        }
-        virtualFiles?.forEach { it ->
-            val node = DefaultMutableTreeNode(it.name)
-            val fileContent = it.inputStream().use { it.bufferedReader().readText() }
-            val classCount = "class".toRegex().findAll(fileContent).count()
-            val functionCount = "fun ".toRegex().findAll(fileContent).count()
-            node.add(DefaultMutableTreeNode("$classCount classes"))
-            node.add(DefaultMutableTreeNode("$functionCount functions"))
-            (treeModel.root as DefaultMutableTreeNode).add(node)
-        }
-        treeModel.reload()
+        val listener = object : VirtualFileListener {
+            override fun beforeContentsChange(event: VirtualFileEvent) {
+                if (event.file.extension.equals("java")) {
+                    val psiFile = PsiManager.getInstance(project).findFile(event.file) ?: return
+                    var classes = 0
+                    var methods = 0
+                    psiFile.accept(object : JavaRecursiveElementVisitor() {
+                        override fun visitClass(aClass: PsiClass?) {
+                            super.visitClass(aClass)
+                            classes++
+                        }
 
-        toolWindow.contentManager.addContentManagerListener(object : ContentManagerListener {
-            override fun contentRemoved(event: ContentManagerEvent) {
-                if (event.content.isCloseable) {
-                    toolWindow.hide(null)
+                        override fun visitMethod(method: PsiMethod?) {
+                            super.visitMethod(method)
+                            methods++
+                        }
+                    })
+                    val root = treeModel.root as DefaultMutableTreeNode
+                    val node = DefaultMutableTreeNode(event.fileName)
+                    node.add(DefaultMutableTreeNode("Classes: $classes"))
+                    node.add(DefaultMutableTreeNode("Methods: $methods"))
+                    val children = root.children()
+                    while (children.hasMoreElements()) {
+                        val child = children.nextElement() as DefaultMutableTreeNode
+                        if (child.userObject == node.userObject) {
+                            treeModel.removeNodeFromParent(child)
+                            break
+                        }
+                    }
+                    treeModel.insertNodeInto(node, root, root.childCount)
                 }
             }
-        })
+        }
+
+        LocalFileSystem.getInstance().addVirtualFileListener(listener)
     }
 
     override fun init(toolWindow: ToolWindow) {
